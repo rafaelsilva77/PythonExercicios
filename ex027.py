18f06e4c4b55708898dbdbde8f1f325661cdb423
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-10-10 03:15:11 -0300

    atualização

---
+++ ex027.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+r1 = nome.split()
+print('Muito prazer em te conhecer!')
+print('O seu primeiro nome é {}'.format(r1[0]))
+print('Seu último nome é {}'.format(nome[len(nome)-1]))
