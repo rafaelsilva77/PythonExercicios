6059ed81c79911028f5019ee4f477aceda43bbbe
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-12-05 03:56:51 -0300

    criei exercício 060

---
+++ ex060.py
@@ -0,0 +1,19 @@
+n = int(input('Digite um número para calacular seu fatorial: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n))
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
+
+
+
+
+'''
+from math import factorial
+n = int(input('Digite um número para calcular seu fatorial: '))
+f = factorial(n)
+print('O fatorial de {} é {}'.format(n, f))'''
