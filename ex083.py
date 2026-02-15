86ab302ab8129d7bfa35700de4f268b2a731a1a8
Rafael dos Santos Silva <githubrafael9@gmail.com>
2026-02-15 04:18:44 -0300

    Criei exercício 83 de Python

---
+++ ex083.py
@@ -0,0 +1,15 @@
+expr = str(input('Digite uma expressão: '))
+pilha = []
+for símb in expr:
+    if símb == '(':
+        pilha.append('(')
+    elif símb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expresão é válida')
+else:
+    print('Sua expresão é inválida')
